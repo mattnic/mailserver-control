452747f6ca644ba60fa13881081359c21d529ebb
Matt Nicholls <git@fwdmsg.co.uk>
2018-09-22 15:08:23 +0100

    Integrated Adminator theme

---
+++ webpack/plugins/index.js
@@ -0,0 +1,21 @@
+const
+  manifest          = require('../manifest');
+
+const plugins = [];
+
+plugins.push(
+  require('./imageminPlugin'),
+  ...(require('./internal')),
+  require('./caseSensitivePlugin'),
+  require('./extractPlugin')
+);
+
+if (manifest.IS_DEVELOPMENT) {
+  plugins.push(require('./dashboardPlugin'));
+}
+
+if (manifest.IS_PRODUCTION) {
+  plugins.push(require('./copyPlugin'));
+}
+
+module.exports = plugins;
